0ed9da47755008dbd7e8e9b239f94d6853fd2537
root-codes <muriithidennis340@gmail.com>
2023-06-19 12:46:28 +0300

    created virtual enviroment

--- Library_management/settings.py
+++ Library_management/settings.py
@@ -134,3 +134,4 @@ LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ekk_onkar/PycharmProjects/Library_management/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
